4a2882dd4a19efde323c9b4197a2775c4e28ee5c
z3ro123 <78100327+z3ro123@users.noreply.github.com>
2021-01-27 12:18:44 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/**********************************************************************
+*HelloWorld
+**********************************************************************
+*Hansung Bae
+*1/27/21
+*CSMC255-003
+**********************************************************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+	
+	System.out.println("Hansung Bae");
+	System.out.println("HelloWorld");
+	System.out.println("Lab 1 of Java");
+	System.out.println("01/27/2021");
+	System.out.println("CSMC255-003");
+	System.out.println();
+	}
+}
